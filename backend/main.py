c59cf74b17087eef15773e0eaaea2432e2979b49
jacobandrist <85521584+jacobandrist@users.noreply.github.com>
2025-11-11 11:22:09 -0600

    created react js

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI, HTTPException
 from typing import Optional
 import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
+
 
 DB_PATH = "nfl.db"
 
 app = FastAPI(title="NFL Last 5 Games API", version="0.1.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000",
+                   "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 def get_db():
