651835326e94eea08f4e0aca677add37dda42b2c
jacobandrist <85521584+jacobandrist@users.noreply.github.com>
2025-11-21 22:10:42 -0600

    num fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,52 @@
 import { Link } from 'react-router-dom'
+import { useEffect, useRef } from 'react'
 
 function Home() {
+  const observerRef = useRef(null)
+
+  useEffect(() => {
+    const counters = document.querySelectorAll('.stat-number')
+    const speed = 100
+
+    const animateCounters = () => {
+      counters.forEach(counter => {
+        const target = +counter.getAttribute('data-target')
+        let count = 0
+        
+        const updateCount = () => {
+          const increment = target / speed
+          
+          if (count < target) {
+            count += increment
+            counter.innerText = Math.ceil(count)
+            requestAnimationFrame(updateCount)
+          } else {
+            counter.innerText = target
+          }
+        }
+        
+        updateCount()
+      })
+    }
+
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        animateCounters()
+      }
+    }, { threshold: 0.3 })
+
+    const leadersSection = document.querySelector('#leaders')
+    if (leadersSection) {
+      observer.observe(leadersSection)
+    }
+
+    return () => {
+      if (leadersSection) {
+        observer.unobserve(leadersSection)
+      }
+    }
+  }, [])
+
   return (
     <>
       <nav>
@@ -25,25 +71,25 @@ function Home() {
         <div className="cards">
           <div className="card">
             <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4431452.png" alt="QB" />
-            <div className="stat-number">2836</div>
+            <div className="stat-number" data-target="2836">0</div>
             <h4>Passing Yards Leader:</h4>
             <p>Drake Maye</p>
           </div>
           <div className="card">
             <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4429795.png" alt="RB" />
-            <div className="stat-number">732</div>
+            <div className="stat-number" data-target="732">0</div>
             <h4>Rushing Yards Leader:</h4>
             <p>Jahmyr Gibbs</p>
           </div>
           <div className="card">
             <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4361432.png" alt="WR" />
-            <div className="stat-number">499</div>
+            <div className="stat-number" data-target="499">0</div>
             <h4>Receiving Yards Leader:</h4>
             <p>Romeo Doubs</p>
           </div>
           <div className="card">
             <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3122132.png" alt="EDGE" />
-            <div className="stat-number">15</div>
+            <div className="stat-number" data-target="15">0</div>
             <h4>Sacks Leader:</h4>
             <p>Myles Garrett</p>
           </div>
